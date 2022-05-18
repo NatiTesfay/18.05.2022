a67bd20bc443e2fe3e97a672cb843ad7f521c22e
NatiTesfay <netaneltes66@gmail.com>
2022-05-18 23:26:41 +0300

    18.05.2022 --js string

---
+++ main.js
@@ -0,0 +1,44 @@
+// function myfunc() {
+//     var text =[2,5,'avi','ron','nati',10];
+//     for (var i = 0;i<text.length;i++) {
+//         if (text[i] * 1 != text[i]) {
+//             text[i]='not number'
+//             console.log(text);
+//         };
+//     }
+// }
+// myfunc();
+
+
+// function mystring() {
+//     var text = 'תעשי תעשי';
+//     for (let i = 0; i < text.length; i++) {
+//        console.log(text[i])   
+//   }
+// }
+// mystring();
+
+// function mystring() {
+//     var text = 'javascript';
+//     for (let i = 0; i < text.length; i++) {
+//         // console.log(text[i]);
+//         // console.log(i);
+//         console.log(text[i]);
+//         console.log(i);
+//     }
+// }
+// mystring();
+
+
+// function newString(strFrom) {
+//     var estring = "";
+//         for (let i = 0; i < estring.length; i++) {    
+//         if (i%2 == 0) {
+//         }
+//         estring += '2';
+//     }
+//         return estring; // if i % 2 it change 2;
+//     }
+// newString(strFrom);
+
+
